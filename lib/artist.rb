4ef9da032a6acd8fa0a0cdd225e1227bf34c5899
Jay Bednar <ironviralmedia@gmail.com>
2018-02-16 02:05:41 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -15,14 +15,14 @@ class Artist
   def add_song(song_name)
     @@song_count += 1
     @songs << song_name
-    song_name.artist = self.name
+    song_name.artist = self
   end
 
   def add_song_by_name(song_name)
     @@song_count += 1
     song = Song.new(song_name)
     @songs << song
-    song.artist = self.name 
+    song.artist = self
   end
 
   def self.song_count
