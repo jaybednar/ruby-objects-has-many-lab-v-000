5d155798c6a5cb6f5933357acbefb738c8bd6eb8
Jay Bednar <ironviralmedia@gmail.com>
2018-02-16 01:43:12 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,10 +1,22 @@
 class Artist
- attr_accessor :name
+  attr_accessor :name
 
 
- def initialize(name)
-   @name = name
-   @@songs = []
- end
- 
+  def initialize(name)
+    @name = name
+    @@songs = []
+  end
+
+  def songs 
+    @@songs
+  end 
+
+  def add_song(song_title)
+    @@songs << song 
+    song.artist = self 
+  end 
+
+  def add_song_by_name(song_title)
+    song = Song.new(song_title)
+   
 end
