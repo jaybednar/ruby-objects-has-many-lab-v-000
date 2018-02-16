6847898a8708de2ab435c169be6f99d9157343f4
Jay Bednar <ironviralmedia@gmail.com>
2018-02-16 02:23:28 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,11 +7,12 @@ class Song
   end
 
   def artist_name
-    if @artist != nil
-      self.artist.name
-    else
-      nil
-    end 
+    nil || self.artist.name
+    # if @artist != nil
+    #   self.artist.name
+    # else
+    #   nil
+    # end
   end
 
 end
